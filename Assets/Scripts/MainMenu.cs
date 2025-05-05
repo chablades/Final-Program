6ab32037b887e09fa627a3c7a979e15dbfc4206d
Cha <57724873+chablades@users.noreply.github.com>
2025-05-04 22:42:14 -0700

    created main menu
    need to fix play and quit button (option is useless)
    
    also set build order

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Stage1-Kingdom"); 
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void QuitGame()
     {
-        
+        Debug.Log("Quit Game"); 
+        Application.Quit();
     }
 }
